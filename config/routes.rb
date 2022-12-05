a5e1dffc10980c7bfd5bcf29961e495ae7d432f8
EC2 Default User <ec2-user@ip-172-31-16-215.ap-northeast-1.compute.internal>
2022-12-05 04:31:39 +0000

    Intial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
